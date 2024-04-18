71d1b5b1f8c5a01c65db86f323f6b704035eaaa3
PrennerProducts <prenner.products@gmail.com>
2024-04-18 18:35:29 +0200

    implement basic Header

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'no-console': 'off', // Dies sollte Konsolenwarnungen deaktivieren, setze es auf "error" oder 2, um sie zu verbieten.
+    'import/no-unresolved': 'error', // Stellt sicher, dass alle Importe aufgelöst werden können
+    'import/no-absolute-path': 'error', // Verbietet absolute Pfade, die zu Fehlern führen könnten
+  },
 }
