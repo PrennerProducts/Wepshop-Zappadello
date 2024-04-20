e76390fa82ab9149c7ecd45b6dd6d554e1cfcde7
PrennerProducts <prenner.products@gmail.com>
2024-04-20 16:15:53 +0200

    Stripe integration, checkout, Orderconfirmation

--- src/app/(pages)/login/order-confirmation/page.tsx
+++ src/app/(pages)/login/order-confirmation/page.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react'
 import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter'
-import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
+import { Gutter } from '../../../_components/Gutter'
+import { mergeOpenGraph } from '../../../_utilities/mergeOpenGraph'
 import { OrderConfirmationPage } from './OrderConfirmationPage'
 
 import classes from './index.module.scss'
