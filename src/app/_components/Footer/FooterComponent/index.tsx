820908d06bf8679e1c33869e5c69a452c9e924a1
PrennerProducts <prenner.products@gmail.com>
2024-07-02 23:51:53 +0200

    comment copyright

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -41,7 +41,7 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
             <Link href="/">
               <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
             </Link>
-            <p className={classes.copyright}>{footer.copyright}</p>
+            {/* <p className={classes.copyright}>{footer.copyright}</p> */}
 
             <div className="classes.socialLinks">
               {navitems.map(item => {
